3d6d4173836f7da50d329d97823537a030202a73
Kolozu <k0pp4ny14@gmail.com>
2026-06-02 08:20:11 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String karakter1  = "Goblin";
+        int K1Hp = 40;
+        int K1Damage = 25;
+        int K1Mana = 45;
+        String K1jel = "○";
+
+        String karakter2 = "Harcos";
+        int K2Hp = 100;
+        int K2Damage = 35;
+        int K2Mana = 50;
+        String K2jel = "▢";
+
+        String karakter3 = "Mágus";
+        int K3Hp = 55;
+        int K3Damage = 30;
+        int K3Mana = 90;
+        String K3jel = "△";
+
+        String karakter4 = "Elf";
+        int K4Hp = 25;
+        int K4Damage = 10;
+        int K4Mana = 35;
+        String K4jel = "◇";
+
+        System.out.println("Válasz a következő karakterek közül: " + karakter1 +", " + karakter2 +", " + karakter3 +", " + karakter4);
+    }
+}
