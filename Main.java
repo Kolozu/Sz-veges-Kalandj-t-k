34a3b3ff712052e5249e3b4b9dbedfbfaa620cc5
Kolozu <k0pp4ny14@gmail.com>
2026-06-04 23:08:25 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+public class Main {
+        String karakter1  = "Goblin";
+        int K1Hp = 40;
+        int K1Damage = 25;
+        int K1Mana = 45;
+        String K1jel = "○";
+
+        String karakter2 = "Harcos";
+        int K2Hp = 100;
+        int K2Damage = 35;
+        int K2Mana = 50;
+        String K2jel = "▢";
+
+        String karakter3 = "Mágus";
+        int K3Hp = 55;
+        int K3Damage = 30;
+        int K3Mana = 90;
+        String K3jel = "△";
+
+        String karakter4 = "Elf";
+        int K4Hp = 25;
+        int K4Damage = 10;
+        int K4Mana = 35;
+        String K4jel = "◇";
+
+        //Ellenfelek
+
+        String Ellen1 = "Troll";
+        int E1Hp = 40;
+        int E1Damage = 20;
+        int E1Mana = 5;
+        String E1jel = "+";
+
+        String Ellen2 = "Csontváz";
+        int E2Hp = 10;
+        int E2Damage = 10;
+        int E2Mana = 10;
+        String E2jel = "-";
+
+        String Ellen3 = "Zombi";
+        int E3Hp = 5;
+        int E3Damage = 10;
+        int E3Mana = 5;
+        String E3jel = "+";
+    }   
